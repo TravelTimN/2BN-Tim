f5aab5fbffe4ca6c29856475cb1385d10421b8a2
Tim Nelson <traveltimn@gmail.com>
2019-01-27 22:11:13 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let wrapper = document.getElementById("wrapper");
+    let topLayer = wrapper.querySelector(".developer");
+    let handle = wrapper.querySelector(".slider");
+    let skew = 0;
+    let delta = 0;
+
+    if (wrapper.className.indexOf("skewed") != -1) {
+        skew = 1000;
+    }
+
+    wrapper.addEventListener("mousemove", function (e) {
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+        handle.style.left = e.clientX + delta + "px";
+        topLayer.style.width = e.clientX + skew + delta + "px";
+    });
+});
