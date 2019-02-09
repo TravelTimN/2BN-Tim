c74efca2bc790f59d3951c77f3b98f131e0f7242
Tim Nelson <TravelTimN@gmail.com>
2019-02-09 14:09:24 +0000

    added navbar, fontAwesome, and lorem for sections

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //-------------
+    // image slider
+    //-------------
     let wrapper = document.getElementById("wrapper");
     let topLayer = wrapper.querySelector(".developer");
     let handle = wrapper.querySelector(".slider");
     let skew = 0;
     let delta = 0;
-
     if (wrapper.className.indexOf("skewed") != -1) {
         skew = 1000;
     }
-
     wrapper.addEventListener("mousemove", function (e) {
         delta = (e.clientX - window.innerWidth / 2) * 0.5;
         handle.style.left = e.clientX + delta + "px";
         topLayer.style.width = e.clientX + skew + delta + "px";
+    });
+    // END image slider
+});
+
+$(document).ready(function () {
+    //$(".navbar li").width((100/$(".navbar li").length)+"%");
+    
+    $("#btn-about").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#about").offset().top - 75
+        }, 100);
+    });
+    $("#btn-projects").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#projects").offset().top - 75
+        }, 100);
+    });
+    $("#btn-skills").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#skills").offset().top - 75
+        }, 100);
+    });
+    $("#btn-contact").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#footer").offset().top - 75
+        }, 100);
     });
 });
