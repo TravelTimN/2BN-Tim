5b8660815853596688e50b8e7291599869d13afa
Tim Nelson <traveltimn@gmail.com>
2019-09-23 21:03:18 +0100

    Refactor/Cleanup + Animate intro landing page/navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,132 +1,108 @@
-document.addEventListener("DOMContentLoaded", function () {
-    //-------------
-    // image slider
-    //-------------
-    let wrapper = document.getElementById("wrapper");
-    let topLayer = wrapper.querySelector(".developer");
-    let handle = wrapper.querySelector(".slider");
-    let skew = 0;
-    let delta = 0;
-    if (wrapper.className.indexOf("skewed") != -1) {
-        skew = 1000;
-    }
-    wrapper.addEventListener("mousemove", function (e) {
-        delta = (e.clientX - window.innerWidth / 2) * 0.5;
-        handle.style.left = e.clientX + delta + "px";
-        topLayer.style.width = e.clientX + skew + delta + "px";
-    });
-});
-
 $(document).ready(function () {
 
+    // image slider after css animation ends
+    $(".skewed .developer").on("animationend", function () {
+        $("header").on("mousemove", function (e) {
+            delta = (e.clientX - window.innerWidth / 2) * 0.5;
+            $(".developer").css("width", e.clientX + 1000 + delta + "px");
+        });
+    });
+
+    // navbar scroll effects
     $(window).on("scroll", function () {
-
-        var about = $("#about").offset().top - 100;
-        var projects = $("#projects").offset().top - 100;
-        var skills = $("#skills").offset().top - 100;
-        var contact = $("#footer").offset().top - 100;
-
+        // toTop button
         if ($(window).scrollTop() > 200) {
-            $("#top").show();
-            $("#top").css({
-                "bottom": ""
+            $("#btn-up").css({
+                "right": "25px",
+                "transform": "rotate(0)"
             });
         } else {
-            $("#top").hide();
-        }
-        if ($(window).scrollTop() <= contact - 10) {
-            $("#bottom").show();
-        } else {
-            $("#bottom").hide();
-            $("#top").css({
-                "bottom": "15px"
+            $("#btn-up").css({
+                "right": "-3em",
+                "transform": "rotate(180deg)"
             });
         }
-
+        let about = $("#about").offset().top - 200;
+        let projects = $("#projects").offset().top - 200;
+        let skills = $("#skills").offset().top - 200;
+        let contact = $("footer").offset().top - 200;
+        // viewport medium and up
         if ($(window).width() >= 600) {
-
+            // about section
             if ($(window).scrollTop() >= about && $(window).scrollTop() <= projects) {
                 $("#btn-about").children(".data-hover").css({
-                    "transform": "rotateX(90deg) translateY(-22px)",
-                    "background-color": "#004377"
+                    "transform": "rotateX(90deg) translateY(-22px)"
                 });
             } else {
                 $("#btn-about").children(".data-hover").css({
-                    "transform": "",
-                    "background-color": ""
+                    "transform": ""
                 });
             }
-
+            // projects section
             if ($(window).scrollTop() >= projects && $(window).scrollTop() <= skills) {
                 $("#btn-projects").children(".data-hover").css({
-                    "transform": "rotateX(90deg) translateY(-22px)",
-                    "background-color": "#004377"
+                    "transform": "rotateX(90deg) translateY(-22px)"
                 });
             } else {
                 $("#btn-projects").children(".data-hover").css({
-                    "transform": "",
-                    "background-color": ""
+                    "transform": ""
                 });
             }
-
+            // skills section
             if ($(window).scrollTop() >= skills && $(window).scrollTop() <= contact) {
                 $("#btn-skills").children(".data-hover").css({
-                    "transform": "rotateX(90deg) translateY(-22px)",
-                    "background-color": "#004377"
+                    "transform": "rotateX(90deg) translateY(-22px)"
                 });
             } else {
                 $("#btn-skills").children(".data-hover").css({
-                    "transform": "",
-                    "background-color": ""
+                    "transform": ""
                 });
             }
-
+            // contact section
             if ($(window).scrollTop() >= contact) {
                 $("#btn-contact").children(".data-hover").css({
-                    "transform": "rotateX(90deg) translateY(-22px)",
-                    "background-color": "#004377"
+                    "transform": "rotateX(90deg) translateY(-22px)"
                 });
             } else {
                 $("#btn-contact").children(".data-hover").css({
-                    "transform": "",
-                    "background-color": ""
+                    "transform": ""
                 });
             }
-
-
-        } else if ($(window).width() < 600) {
+        } else {
+            // about section
             if ($(window).scrollTop() >= about && $(window).scrollTop() <= projects) {
                 $("#btn-about").children(".data-hover").css({
-                    "background-color": "#1BA9DD"
+                    "background-color": "rgba(var(--blueDark1), 1)"
                 });
             } else {
                 $("#btn-about").children(".data-hover").css({
                     "background-color": ""
                 });
             }
+            // projects section
             if ($(window).scrollTop() >= projects && $(window).scrollTop() <= skills) {
                 $("#btn-projects").children(".data-hover").css({
-                    "background-color": "#1BA9DD"
+                    "background-color": "rgba(var(--blueDark1), 1)"
                 });
             } else {
                 $("#btn-projects").children(".data-hover").css({
                     "background-color": ""
                 });
             }
-
+            // skills section
             if ($(window).scrollTop() >= skills && $(window).scrollTop() <= contact) {
                 $("#btn-skills").children(".data-hover").css({
-                    "background-color": "#1BA9DD"
+                    "background-color": "rgba(var(--blueDark1), 1)"
                 });
             } else {
                 $("#btn-skills").children(".data-hover").css({
                     "background-color": ""
                 });
             }
-
+            // contact section
             if ($(window).scrollTop() >= contact) {
                 $("#btn-contact").children(".data-hover").css({
-                    "background-color": "#1BA9DD"
+                    "background-color": "rgba(var(--blueDark1), 1)"
                 });
             } else {
                 $("#btn-contact").children(".data-hover").css({
@@ -136,15 +112,10 @@ $(document).ready(function () {
         }
     });
 
-
-    $("#top").click(function () {
+    // smooth scroll to sections
+    $("#btn-up").click(function () {
         $("html, body").animate({
             scrollTop: $("#toTop").offset().top
-        }, 100);
-    });
-    $("#bottom").click(function () {
-        $("html, body").animate({
-            scrollTop: $("#footer").offset().top - 75
         }, 100);
     });
     $("#btn-about").click(function () {
@@ -164,8 +135,30 @@ $(document).ready(function () {
     });
     $("#btn-contact").click(function () {
         $("html, body").animate({
-            scrollTop: $("#footer").offset().top - 75
+            scrollTop: $("footer").offset().top - 75
         }, 100);
     });
+
+    // animate <developer> in header
+    setTimeout(function () {
+        let developer = ["<developer>"];
+        let typeChar = function () {
+            $("#developer").append(word[char++]);
+            if (char < word.length) {
+                setTimeout(typeChar, 100);
+            } else {
+                typeWord();
+            }
+        };
+        let i = 0;
+        let typeWord = function () {
+            if (i < developer.length) {
+                word = developer[i++];
+                char = 0;
+                typeChar();
+            }
+        };
+        typeWord();
+    }, 500);
 
 });
