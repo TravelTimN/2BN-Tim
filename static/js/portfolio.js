a5dabe09601a279c98ae3c570b86e1e761de76b3
Tim Nelson <traveltimn@gmail.com>
2019-10-14 19:21:46 +0100

    Update Portfolio

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -2,64 +2,74 @@ $(document).ready(() => {
 
     // portfolio details
     let portfolio = [{
+            title: "Paul M Music",
             course: "Code Institute",
             project: "Milestone-01",
             name: "User-Centric Frontend Development",
             image: "milestone01",
-            title: "Paul M Music",
             repo: "https://github.com/TravelTimN/ci-milestone01-ucfd",
             link: "https://traveltimn.github.io/ci-milestone01-ucfd",
             "chips": ["HTML", "CSS", "JavaScript"]
         },
         {
+            title: "IATA Map",
             course: "Code Institute",
             project: "Milestone-02",
             name: "Interactive Frontend Development",
             image: "milestone02",
-            title: "IATA Map",
             repo: "https://github.com/TravelTimN/ci-milestone02-ifd",
             link: "https://traveltimn.github.io/ci-milestone02-ifd",
             "chips": ["HTML", "CSS", "JavaScript", "jQuery", "LeafletJS"]
         },
         {
+            title: "2BN-Desserts",
             course: "Code Institute",
             project: "Milestone-04",
             name: "Data-Centric Development",
             image: "milestone04",
-            title: "2BN-Desserts",
             repo: "https://github.com/TravelTimN/ci-milestone04-dcd",
             link: "https://desserts-2bn.herokuapp.com",
             "chips": ["HTML", "CSS", "JavaScript", "jQuery", "Materialize", "Heroku", "Python", "Flask", "MongoDB"]
         },
         {
+            title: "Unicorn Attractor",
             course: "Code Institute",
             project: "Milestone-05",
             name: "Full-Stack Frameworks",
             image: "milestone05",
-            title: "Unicorn Attractor",
             repo: "https://github.com/TravelTimN/ci-milestone05-fsfw",
             link: "https://unicorn-attractor-2bn.herokuapp.com",
             "chips": ["HTML", "CSS", "JavaScript", "jQuery", "Materialize", "Heroku", "Python", "Django", "PostgreSQL", "Stripe", "AWS", "TravisCI"]
         },
         {
+            image: "bearly-escaped",
+            title: "Bearly Escaped",
             course: "",
             project: "",
             name: "Bearly Escaped",
-            image: "bearly-escaped",
-            title: "Bearly Escaped",
             repo: "https://github.com/TravelTimN/bearly-escaped",
             link: "https://traveltimn.github.io/bearly-escaped",
             "chips": ["HTML", "CSS", "JavaScript", "jQuery"]
         },
         {
+            title: "Bauhaus100",
             course: "",
             project: "",
             name: "Bauhaus100",
             image: "bauhaus100",
-            title: "Bauhaus100",
             repo: "https://github.com/TravelTimN/bauhaus100",
             link: "https://traveltimn.github.io/bauhaus100",
             "chips": ["HTML", "CSS"]
+        },
+        {
+            title: "Christmas 2013",
+            course: "",
+            project: "Learning Adobe After Effects",
+            name: "Bitstrips Christmas Greeting Animation",
+            image: "ae2013",
+            repo: "",
+            link: "https://youtu.be/7RUtq_EOrJg",
+            "chips": ["Photoshop", "AfterEffects", "YouTube"]
         }
     ];
 
@@ -72,11 +82,24 @@ $(document).ready(() => {
 
     // map each project to own <li> item
     portfolio.map(project => {
-        li_project += "<li><div class='divider blue'></div><h2 class='center-align'>" + project.title + "</h2>";
-        li_project += "<figure class='fadeUp'><img src='" + imgs_portfolio + project.image + imgType + "' alt='" + project.title + "' lazyload='on'>";
-        li_project += "<figcaption><h2>" + project.course + "</h2><h4>" + project.project + "</h4><h3>" + project.name + "</h3>";
-        li_project += "<a href='" + project.repo + "' class='btn' target='_blank'><i class='fab fa-github right' aria-hidden='true'></i>REPO</a>";
-        li_project += "<a href='" + project.link + "' class='btn' target='_blank'><i class='fas fa-link right' aria-hidden='true'></i>LIVE</a>";
+        li_project += "<li><div class='divider blue'></div>";
+        li_project += "<h2 class='center-align'>" + project.title + "</h2>";
+        li_project += "<figure class='fadeUp'><img src='" + imgs_portfolio + project.image + imgType + "' alt='" + project.title + "' lazyload='on'><figcaption>";
+        if (project.course) {
+            li_project += "<h2>" + project.course + "</h2>";
+        }
+        if (project.project) {
+            li_project += "<h4>" + project.project + "</h4>";
+        }
+        if (project.name) {
+            li_project += "<h3>" + project.name + "</h3>";
+        }
+        if (project.repo) {
+            li_project += "<a href='" + project.repo + "' class='btn' target='_blank'><i class='fab fa-github right' aria-hidden='true'></i>REPO</a>";
+        }
+        if (project.link) {
+            li_project += "<a href='" + project.link + "' class='btn' target='_blank'><i class='fas fa-link right' aria-hidden='true'></i>LIVE</a>";
+        }
         li_project += "</figcaption></figure><ul class='chip-container center-align'>";
         // create 'chip' <div> for each tool used
         for (var i in project.chips) {
